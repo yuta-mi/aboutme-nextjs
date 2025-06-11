b8ee0084f13daf97e49251880260ec5dc11d5d68
yuta-pcodes <yuta@p-codes.dev>
2025-06-12 04:57:50 +0900

    いったん自己紹介ページを作成

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <footer className="0">
+      <small className="text-sm">© 2025 Yuta Miura</small>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  return (
+    <header className="w-5/6 mx-auto max-w-screen-xl border-b flec-col border-amber-50 min-w-screen flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Yuta's Profile</h1>
+      <small className="text-sm ml-4">
+        鋭意作成中
+      </small>
+    </header>
+  );
+};
+
+export default Header;
